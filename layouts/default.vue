4326194b8cc940e9f46a1ef6ce8876474dd01185
Diego Reyes <diegorele13@gmail.com>
2023-11-17 18:05:57 -0500

    set: header

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import TheHeader from '@/layouts/web/TheHeader.vue';
+
+</script>
+
+
+<template>
+  <div class="app-container">
+    <TheHeader />
+    <div class="app-container-view">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.app-container {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  &-view {
+    flex: 1;
+  }
+}
+</style>
---
+++ layouts/web/TheHeader.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import CrushHeader from '@nabux-crush/crush-header';
+import KyriosLogo from '@/assets/kyrios.png'
+
+const menuItems = [
+  {
+    link: '/',
+    name: 'inicio'
+  },
+  {
+    link: '/',
+    name: 'Servicios'
+  },
+  {
+    link: '/',
+    name: 'Boletines'
+  },
+  {
+    link: '/',
+    name: 'Contacto'
+  },
+  {
+    link: '/',
+    name: 'Registrarte'
+  },
+  {
+    link: '/',
+    name: 'Empezar diagnóstico'
+  },
+]
+</script>
+
+<template>
+  <header class="container">
+    <CrushHeader
+      brandName="kyrios"
+      :logoSrc="KyriosLogo">
+      <template 
+        #menu-buttons>
+        <RouterLink 
+          v-for="(item, index) in menuItems" 
+          :key="index" 
+          :to="item.link" 
+          :style="{ color: 'black', textDecoration: 'none' }"
+          class="container-buttons">
+          {{item.name}}
+        </RouterLink>
+      </template>
+
+    </CrushHeader>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+:deep(.header) {
+  background-color: $white;
+  gap: 12px;
+}
+:deep(.header-icon) {
+  color: $black;
+}
+:deep(.header-figure-logo) {
+  width: 140px;
+  object-fit: inherit;
+}
+:deep(.toggle-menu-header) {
+  padding: 12px;
+}
+:deep(.header-buttons :nth-child(5)) {
+  padding: 6px 12px;
+  border: 2px solid $blue;
+  border-radius: 8px;
+}
+:deep(.header-buttons :nth-child(6)) {
+  padding: 6px 12px;
+  background-color: $blue;
+  color: $white !important;
+  border-radius: 8px;
+  text-align: center;
+}
+.container {
+  &-buttons {
+    font-weight: 700;
+    color: $black !important;
+  }
+}
+</style>
