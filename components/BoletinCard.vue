b94f829350f64d79078cf905bfa71c820a27ba9b
Diego Reyes <diegorele13@gmail.com>
2023-11-18 16:09:34 -0500

    add: boletines page

---
+++ components/BoletinCard.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  imageUrl: String,
+  frameNumber: {
+    type: String,
+    required: false,
+  },
+  title: String,
+  date: String,
+  link: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+
+<template>
+  <div class="card-container">
+    <div class="card-image" :style="{ backgroundImage: 'url(' + imageUrl + ')' }">
+      <div
+        v-if="frameNumber?.length" 
+        class="frame-number">{{ frameNumber }}</div>
+    </div>
+    <div class="card-details">
+      <h2 class="card-title">{{ title }}</h2>
+      <p class="card-date">{{ date }}</p>
+      <div class="card-button">
+        <RouterLink :to="link" class="button">Ver detalle</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.card-container {
+  border-radius: 15px;
+  overflow: hidden;
+  width: 300px;
+}
+
+.card-image {
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  height: 150px; /* Adjust based on your image */
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  padding: 10px;
+
+  .frame-number {
+    background-color: rgba(255, 255, 255, 0.5);
+    padding: 5px 10px;
+    border-radius: 10px;
+    margin: 0;
+  }
+}
+
+.card-details {
+  padding: 20px;
+  text-align: center;
+
+  .card-title {
+    margin: 0;
+    color: $black;
+    font-size: 18px;
+  }
+
+  .card-date {
+    color: $black;
+    font-size: 16px;
+  }
+
+  .card-button {
+    margin-top: 15px;
+
+    .button {
+      background-color: $blue; 
+      color: white;
+      border: none;
+      padding: 10px 20px;
+      border-radius: 20px;
+      cursor: pointer;
+      transition: background-color 0.3s;
+      text-decoration: none;
+
+      &:hover {
+        background-color: darken($blue, 10%);
+      }
+    }
+  }
+}
+</style>
---
+++ pages/boletines/index.vue
@@ -0,0 +1,83 @@
+<script setup>
+import empresarios from '@/assets/hero/empresarios.jpg'
+
+const boletines = [
+  {
+    imageUrl: empresarios,
+    title: 'Nombre o título del boletín',
+    date: 'Noviembre 01, 2023',
+    link: '/' 
+  },
+  {
+    imageUrl: empresarios,
+    title: 'Nombre o título del boletín',
+    date: 'Noviembre 01, 2023',
+    link: '/' 
+  },
+  {
+    imageUrl: empresarios,
+    title: 'Nombre o título del boletín',
+    date: 'Noviembre 01, 2023',
+    link: '/' 
+  },
+  {
+    imageUrl: empresarios,
+    title: 'Nombre o título del boletín',
+    date: 'Noviembre 01, 2023',
+    link: '/' 
+  },
+  {
+    imageUrl: empresarios,
+    title: 'Nombre o título del boletín',
+    date: 'Noviembre 01, 2023',
+    link: '/' 
+  },
+];
+
+</script>
+
+<template>
+  <div class="container">
+    <p class="container-title">Boletines</p>
+    <p class="container-text">
+      Mantente al tanto de la información sobre estándares, regulaciones y mejores prácticas internacionales para alcanzar tus objetivos de certificación y mejora de procesos
+    </p>
+    <div class="container-tickets">
+      <BoletinCard
+      v-for="(boletin, index) in boletines"
+      :key="index"
+      :imageUrl="boletin.imageUrl"
+      :title="boletin.title"
+      :date="boletin.date"
+      :link="boletin.link" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin: 40px 0;
+  padding: 24px;
+  &-title {
+    color: $black;
+    font-size: $h2-font-size;
+    text-align: center;
+    font-weight: 700;
+  }
+  &-text {
+    font-size: $body-font-size;
+    text-align: center;
+    padding: 24px;
+    @media (min-width: $tablet-upper-breakpoint) {
+      font-size: $h2-font-size;
+    }
+  }
+  &-tickets {
+    display: flex;
+    margin-top: 40px;
+    flex-wrap: wrap;
+    gap: 24px;
+    justify-content: center;
+  }
+}
+</style>
