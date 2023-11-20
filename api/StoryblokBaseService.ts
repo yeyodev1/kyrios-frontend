c393faed276eac8c3595420a63478fb7be89bb6c
Diego Reyes <diegorele13@gmail.com>
2023-11-20 15:03:37 -0500

    set token to access api

--- api/StoryblokBaseService.ts
+++ api/StoryblokBaseService.ts
@@ -3,7 +3,7 @@ import StoryblokClient from 'storyblok-js-client';
 const baseURL = 'https://api-us.storyblok.com/v2'
 
 const Storyblok = new StoryblokClient({
-  accessToken: 'AhQR1WFGC3nEJEeDsjlDLAtt',
+  accessToken: 'FNjolXe0RQeP3EWIGPqocAtt',
   cache: {
     clear: 'auto',
     type: 'memory'
