733fbf48b6d0437d21569f9927986d2a6c638aa7
Diego Reyes <diegorele13@gmail.com>
2023-11-27 20:23:12 -0500

    done: question-view

--- pages/tests/[test]/index.vue
+++ pages/tests/[test]/index.vue
@@ -1,3 +1,139 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+import useTestStore from '@/store/TestStore';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const testStore = useTestStore();
+
+const testId = route.params.test; 
+
+const currentQuestionIndex = ref(0);
+const selectedTest = computed(() => testStore.tests.find(test => test.id === testId));
+const responseOptions = computed(() => testStore.responseOptions);
+const currentQuestion = computed(() => {
+  return selectedTest.value?.satisfactionQuestions[currentQuestionIndex.value];
+});
+const isCurrentQuestionAnswered = computed(() => {
+  return currentQuestion.value?.response !== null;
+});
+const isLastQuestion = computed(() => {
+  return currentQuestionIndex.value === selectedTest.value.satisfactionQuestions.length - 1;
+});
+
+function setResponse(testId, questionIndex, value) {
+  testStore.setResponse(testId, questionIndex, value);
+}
+
+function nextQuestion() {
+  if (currentQuestionIndex.value < selectedTest.value.satisfactionQuestions.length - 1) {
+    currentQuestionIndex.value++;
+  } else {
+    console.log('holi')
+  }
+}
+function isOptionSelected(value) {
+  return currentQuestion.value?.response?.value === value;
+}
+</script>
+
 <template>
-  aqui se vera cada examen
+  <div class="container">
+    <div 
+      v-if="selectedTest" 
+      class="container-test">
+      <h2 class="container-test-title">{{ selectedTest.name }}</h2>
+      <p class="container-test-description">{{ selectedTest.description }}</p>
+
+      <div 
+        v-if="currentQuestion" 
+        class="container-test-question-container">
+        <p class="container-test-question-container-question">
+          {{ currentQuestion.question }}
+        </p>
+        <div class="container-test-question-container-response-options">
+          <button
+            v-for="option in responseOptions"
+            :key="option.value"
+            @click="setResponse(selectedTest.id, currentQuestionIndex, option.value)"
+            :class="{ 'selected': isOptionSelected(option.value) }"
+            class="container-test-question-container-response-options-option-button" >
+            {{ option.value }}
+          </button>
+        </div>
+      </div>
+
+      <button 
+        v-if="currentQuestionIndex < selectedTest.satisfactionQuestions.length" 
+        @click="nextQuestion" 
+        :disabled="!isCurrentQuestionAnswered"
+        class="container-test-next-button">
+        {{ isLastQuestion ? 'Finalizar' : 'Siguiente' }}
+      </button>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  max-width: 800px;
+  margin: 20px auto;
+  padding: 20px;
+  border-radius: 8px;
+  &-test {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    &-title {
+      text-align: center;
+    }
+    &-description {
+      font-size: 1rem; 
+    }
+    &-question-container {
+      margin: 20px 0;
+      &-question {
+        margin-bottom: 20px;
+        font-size: $body-font-size;
+        color: $blue;
+        font-weight: 500;
+      }
+      &-response-options {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 20px;
+        &-option-button {
+          padding: 10px 20px;
+          border-radius: 4px;
+          cursor: pointer;
+          border: 1px solid $grey;
+          transition: background-color 0.3s ease;
+          &:hover,
+          &.selected { 
+            background-color: $light-blue;
+          }
+        }
+      }
+    }
+    &-next-button {
+      padding: 10px 20px;
+      border: none;
+      border-radius: 4px;
+      background-color: $blue;
+      color: white;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+  
+      &:hover {
+        background-color: darken($blue, 20%);
+      }
+      &:disabled {
+        background-color: #ccc;
+        cursor: not-allowed;
+      }
+    }
+  }
+}
+</style>
--- store/TestStore.ts
+++ store/TestStore.ts
@@ -5,6 +5,23 @@ interface Test {
   name: string;
   description: string;
 }
+interface ResponseOption {
+  value: number;
+  description: string;
+}
+
+interface SatisfactionQuestion {
+  question: string;
+  response: ResponseOption | null;
+}
+
+const responseOptions: ResponseOption[] = [
+  { value: 0, description: "Sin documentar ni implementar" },
+  { value: 1, description: "Implementado pero no formalizado ni comunicado" },
+  { value: 2, description: "Implementado, documentado pero requiere mejorar" },
+  { value: 3, description: "Implementado listo para auditar" },
+  { value: 4, description: "No aplica" },
+];
 
 const useTestStore = defineStore("testStore", {
 	state: () => ({
@@ -14,21 +31,124 @@ const useTestStore = defineStore("testStore", {
 				name: "ISO 27001",
 				description:
 					"Seguridad de la información y la gestión de riesgos.",
+				satisfactionQuestions: [
+          {
+            question: "Pregunta 1",
+            response: null,
+          },
+          {
+            question: "Pregunta 2",
+            response: null,
+          },
+          {
+            question: "Pregunta 3",
+            response: null,
+          },
+          {
+            question: "Pregunta 4",
+            response: null,
+          },
+          {
+            question: "Pregunta 5",
+            response: null,
+          },
+          {
+            question: "Pregunta 6",
+            response: null,
+          },
+          {
+            question: "Pregunta 7",
+            response: null,
+          },
+          {
+            question: "Pregunta 8",
+            response: null,
+          },
+        ],	
 			},
 			{
 				id: "iso22301",
 				name: "ISO 22301",
 				description:
 					"Continuidad de negocios y la gestión de la resiliencia.",
+				satisfactionQuestions: [
+          {
+            question: "Pregunta 1",
+            response: null,
+          },
+          {
+            question: "Pregunta 2",
+            response: null,
+          },
+          {
+            question: "Pregunta 3",
+            response: null,
+          },
+          {
+            question: "Pregunta 4",
+            response: null,
+          },
+          {
+            question: "Pregunta 5",
+            response: null,
+          },
+          {
+            question: "Pregunta 6",
+            response: null,
+          },
+          {
+            question: "Pregunta 7",
+            response: null,
+          },
+          {
+            question: "Pregunta 8",
+            response: null,
+          },
+        ],
 			},
 			{
 				id: "iso20000",
 				name: "ISO 20000",
 				description:
 					"Gestión de servicios de tecnologías de la información.",
+				satisfactionQuestions: [
+          {
+            question: "Pregunta 1",
+            response: null,
+          },
+          {
+            question: "Pregunta 2",
+            response: null,
+          },
+          {
+            question: "Pregunta 3",
+            response: null,
+          },
+          {
+            question: "Pregunta 4",
+            response: null,
+          },
+          {
+            question: "Pregunta 5",
+            response: null,
+          },
+          {
+            question: "Pregunta 6",
+            response: null,
+          },
+          {
+            question: "Pregunta 7",
+            response: null,
+          },
+          {
+            question: "Pregunta 8",
+            response: null,
+          },
+        ],
 			},
 		],
-    selectedTest: null as Test | null
+		responseOptions,
+    selectedTest: null as Test | null,
 	}),
   actions: {
     selectTest(testId: string) {
@@ -38,7 +158,14 @@ const useTestStore = defineStore("testStore", {
       } else {
         console.error('test not found'); 
       }
-    }
+    },
+		setResponse(testId: string, questionIndex: number, responseValue: number) {
+      const test = this.tests.find(t => t.id === testId);
+      const responseOption = this.responseOptions.find(option => option.value === responseValue) || null;
+      if (test && typeof test.satisfactionQuestions[questionIndex] !== 'undefined') {
+        test.satisfactionQuestions[questionIndex].response = responseOption;
+      }
+    },
   }
 });
 
