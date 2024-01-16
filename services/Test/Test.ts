685e2606121a9a58ea4b17b1f391ed307e15b0b0
Diego Reyes <diegorele13@gmail.com>
2024-01-15 21:32:16 -0500

    Remove unused method getLastTest() from APITest class

--- services/Test/Test.ts
+++ services/Test/Test.ts
@@ -6,10 +6,6 @@ class APITest extends APIBase {
   async createTest(testData: any): Promise<AxiosResponse<any>> {
     return this.post ('isotest/create', testData);
   }
-
-  async getLastTest(userId: string): Promise<AxiosResponse<any>>{
-    return this.get(`isotest/lastTest/${userId}`);
-  }
 }
 
 export default APITest;
