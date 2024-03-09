d2da2ed1ca49c872608d4b33e3cceefae658ba8b
Diego Reyes <diegorele13@gmail.com>
2024-03-09 17:46:05 -0500

    add: component

---
+++ components/teamCard.vue
@@ -0,0 +1,56 @@
+<script setup> 
+
+const props = defineProps({
+  imageUrl: {
+    type: String, 
+    required: false
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+    <img 
+      :src="imageUrl"
+      alt="imagen de perfil"
+      class="card-image">
+    <h3 class="card-name">Nombre: <span class="card-name-span">{{ name }}</span></h3>
+    <p class="card-rol">Rol: <span class="card-rol-span">{{ role }}</span></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    &-image {
+      border-radius: 50%;
+      width: 100px;
+      height: 100px;
+      object-fit: contain;
+    }
+    &-rol {
+      font-weight: bold;
+      font-size: 1rem;
+      &-span {
+        font-weight: 400;
+      }
+    }
+    &-name {
+      font-size: 1rem;
+      &-span {
+        font-weight: bold;
+      }
+    }
+  }
+</style>
---
+++ pages/team/index.vue
@@ -0,0 +1,40 @@
+<script setup>
+import yulyImage from '@/assets/yuly.png'
+
+const team = [
+  {
+    id: 1,
+    imageUrl: yulyImage,
+    name: 'Yuly Alexandra Mendoza Bailón',
+    role: `SISTEMA DE GESTIÓN DE LA CALIDAD CERTIFICADO ISO 9001:2015 ALCANCE: SERVICIOS DE CAPACITACIÓN, ASESORÍA EN SISTEMAS DE GESTIÓN Y MEJORA CONTINUA. CERTIFICADO SGS EC19/81841649`
+  }
+]
+</script>
+
+<template>
+  <div class="container">
+    <h3 class="container-title">Equipo</h3>
+    <div class="container-team-cards">
+      <teamCard v-for="member in team" :key="member.id" :imageUrl="member.imageUrl" :name="member.name" :role="member.role" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 2rem;
+  &-title {
+    font-size: 2rem;
+  }
+  &-team-cards {
+    max-width: 600px;
+    display: flex;
+    gap: 2rem;
+  }
+}
+</style>
