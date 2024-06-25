1cb829d5f1c014259be769126d026e22ab3643d4
Jorge DC <jddelcampom@gmail.com>
2024-06-25 16:10:48 -0500

    Add new menu items util for the header

---
+++ utils/HeaderItems.ts
@@ -0,0 +1,109 @@
+const subItemsForServices = [
+  {
+    link: '/services#capacitacion',
+    name: 'Capacitación y auditoria',    
+    id: 'capacitacion'
+  },
+  {
+    link: '/services#implementacion',
+    name: 'Implementación y auditoría',
+    id: 'implementacion'
+  },
+  {
+    link: '/services#reporte',
+    name: 'Reporte de sostenibilidad',
+    id: 'reporte'
+  },
+  {
+    link: '/services#capacitacion',
+    name: 'Capacitación y asesoría',
+    id: 'capacitacion'
+  },
+  {
+    link: '/services#gestion',
+    name: 'Gestión de riesgos',
+    id: 'gestion'
+  },
+  {
+    link: '/services#economia',
+    name: 'Economía circular',
+    id: 'economia'
+  },
+  {
+    link: '/services#proteccion',
+    name: 'Protección de datos',
+    id: 'proteccion'
+  },
+  {
+    link: '/services#administracion',
+    name: 'Administración de bodegas',
+    id: 'administracion'
+  },
+  {
+    link: '/services#gerencia',
+    name: 'Gerencia estratégica de costos ABC',
+    id: 'gerencia'
+  },
+  {
+    link: '/services#analisis',
+    name: 'Análisis y mejora de procesos',
+    id: 'analisis'
+  },
+  {
+    link: '/services#lean',
+    name: 'LEAN Production',
+    id: 'lean'
+  },
+  {
+    link: '/services#mejora',
+    name: 'Mejora de servicio al cliente',
+    id: 'mejora'
+  },
+  {
+    link: '/services#gestion',
+    name: 'Gestión de proyectos',
+    id: 'gestion'
+  },
+  {
+    link: '/services#itil',
+    name: 'ITIL',
+    id: 'itil'
+  },
+  {
+    link: '/services#formador',
+    name: 'Formador de formadores',
+    id: 'formador'
+  }
+]
+
+export const allMenuItems = [
+  {
+    link: '/team', 
+    name: 'Equipo'
+  },
+  {
+    link: '/services',
+    name: 'Servicios',
+    subItems: subItemsForServices
+  },
+  {
+    link: '/boletines',
+    name: 'Boletines'
+  },
+  {
+    link: '/methodology',
+    name: 'Metodología'
+  },
+  {
+    link: '/about',
+    name: 'Conocenos'
+  },
+  {
+    link: '/userRegister',
+    name: 'Registrarte'
+  },
+  {
+    link: '/userlogin',
+    name: 'Empezar diagnóstico'
+  },  
+]
